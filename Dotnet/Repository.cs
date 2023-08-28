56c270a6a0c69b6d7b6b53bc0975034797616fb4
VigneshKanna <35601923+Vigneshkna@users.noreply.github.com>
2023-08-28 12:14:28 +0100

    Add files via upload

---
+++ Dotnet/Repository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using NdcPlanning.App_Start;                // State
+using MisModel.Warehouse;                   // Reason
+using Mis.Model.Warehouse;                  // Department
+
+
+namespace NdcPlanning
+{
+    public class Repository : INdcPlanningRepository
+    {
+        public Department GetT85Department()
+        {
+            // TODO: Remove use / use real data from database
+            return new Department { Id = 11, DepartmentCode = "T85", DepartmentName = "????", ApproxSinglesPerTrailer = 1000 };
+        }
+
+        public int GetUsersPickSiteId()
+        {
+            return 670; // Swindon
+        }
+
+        public int GetUsersCrossdockSiteId()
+        {
+            return 666; // Neasden
+        }
+
+        public decimal GetTrailerFillPerRpe()
+        {
+            return (1m / 99m);
+        }
+
+    }
+}
